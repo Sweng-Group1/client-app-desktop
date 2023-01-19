5a452b538c2ee30b0040b0adf4135a9b0b757e20
Fraser Todd <flt515@york.ac.uk>
2023-01-19 15:01:03 +0000

    added UploadSceneTest

--- src/main/java/sweng/group/one/client_app_desktop/sceneControl/UploadScene.java
+++ src/main/java/sweng/group/one/client_app_desktop/sceneControl/UploadScene.java
@@ -1,5 +1,7 @@
 package sweng.group.one.client_app_desktop.sceneControl;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -16,12 +18,19 @@ public class UploadScene extends JPanel {
 	public UploadScene() {
 		super();
 		uploadMediaPanel = new JPanel();
+		this.add(uploadMediaPanel, BorderLayout.WEST);
+		
 		previewPanel = new JPanel();
+		this.add(previewPanel);
 		
 		describePostPanel = new JPanel();
 		describePostTextField = new JTextField();
+		describePostPanel.add(describePostTextField);
+		this.add(describePostPanel, BorderLayout.EAST);
 		
 		tagPanel = new JPanel();
-		
+		tagTextField = new JTextField();
+		tagPanel.add(tagTextField);
+		this.add(tagPanel, BorderLayout.EAST);
 	}
 }
---
+++ src/test/java/sweng/group/one/client_app_desktop/sceneControl/UploadSceneTest.java
@@ -0,0 +1,7 @@
+package sweng.group.one.client_app_desktop.sceneControl;
+
+import junit.framework.TestCase;
+
+public class UploadSceneTest extends TestCase{
+
+}
