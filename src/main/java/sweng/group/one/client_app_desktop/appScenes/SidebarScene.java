f4883a1afa40c92a1ff4a10f00e63afb26f41c82
ProtonNumber <jwhinton@yenno.com>
2023-01-19 14:46:38 +0000

    Added getter for isOpen

--- src/main/java/sweng/group/one/client_app_desktop/appScenes/SidebarScene.java
+++ src/main/java/sweng/group/one/client_app_desktop/appScenes/SidebarScene.java
@@ -1,5 +1,17 @@
 package sweng.group.one.client_app_desktop.appScenes;
 
 public class SidebarScene {
-
+	private boolean isOpen;
+	public void open() {
+		
+	}
+	public void close() {
+		
+	}
+	public boolean search(String search) {
+		return true;
+	}
+	public boolean isOpen() {
+		return isOpen;
+	}
 }
