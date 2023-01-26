96fd7dfe005b59e018fb2604ff46aeaf628d32eb
ProtonNumber <jwhinton@yenno.com>
2023-01-26 14:42:04 +0000

    Created basic sidebar, got trapped in dependency hell

--- src/main/java/sweng/group/one/client_app_desktop/App.java
+++ src/main/java/sweng/group/one/client_app_desktop/App.java
@@ -1,5 +1,8 @@
 package sweng.group.one.client_app_desktop;
 
+import javax.swing.JFrame;
+
+import sweng.group.one.client_app_desktop.appScenes.SidebarScene;
 
 /**
  * Hello world!
@@ -9,6 +12,8 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
+    	JFrame f=new JFrame("Unnamed App");
+    	SidebarScene sidebar = new SidebarScene();
+    	f.add(sidebar);
     }
 }
--- src/main/java/sweng/group/one/client_app_desktop/appScenes/SidebarScene.java
+++ src/main/java/sweng/group/one/client_app_desktop/appScenes/SidebarScene.java
@@ -1,12 +1,55 @@
 package sweng.group.one.client_app_desktop.appScenes;
 
-public class SidebarScene {
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class SidebarScene extends JPanel {
+	private static final long serialVersionUID = 1L;
 	private boolean isOpen;
-	public void open() {
+	
+	// Top of the side bar, containing search bar and minimise button
+	static JPanel top;
+	
+	// Lower portion, containing presentation thumbnails
+	static JPanel thumbs;
+	
+	static JTextField searchBar;
+	static JButton minimise;
+	
+	public SidebarScene() {
+		top = new JPanel(new FlowLayout());
 		
+		minimise = new JButton("_");
+		minimise.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				close();
+				
+			}
+		});
+		
+		top.add(minimise);
+		top.add(searchBar);
+		
+		this.setLayout(new BorderLayout());
+		this.add(top, BorderLayout.NORTH);
+		this.add(thumbs, BorderLayout.CENTER);
+	}
+	
+	public void open() {
+		isOpen = true;
+		this.setVisible(true);
 	}
 	public void close() {
-		
+		isOpen = false;
+		this.setVisible(false);
 	}
 	public boolean search(String search) {
 		return true;
