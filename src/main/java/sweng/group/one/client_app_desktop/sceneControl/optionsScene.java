a8f041705c5d01aed2e477f920fcee858c9f874c
Oli Partridge <op642@york.ac.uk>
2023-01-26 15:26:10 +0000

    Update optionsScene.java

--- src/main/java/sweng/group/one/client_app_desktop/sceneControl/optionsScene.java
+++ src/main/java/sweng/group/one/client_app_desktop/sceneControl/optionsScene.java
@@ -1,7 +1,30 @@
 package sweng.group.one.client_app_desktop.sceneControl;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class optionsScene extends JPanel {
+	private Boolean isOpen;
+	
+	private JPanel open;
+	private Jpanel close;
+	
+	// change these to boolean to test, can be made into JButton
+	// When functions are ready
+	private JButton accountButton;
+	private JButton addPost;
+	private JButton helpButton;
+	private JButton closeButton;
+	
+	Public optionsScene() {
+		super();
+		
+		open = new JPanel();
+		
+		
+		accountButton = new JButton();
+	}
+	
+	
 	
 
 }
