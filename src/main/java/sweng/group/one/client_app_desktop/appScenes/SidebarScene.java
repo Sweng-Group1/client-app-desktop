f1cee43ab6cb4ac2ca2e3d05040878d13688868b
ProtonNumber <jwhinton@yenno.com>
2023-02-02 14:49:09 +0000

    Added function prototypes for goto and replace

--- src/main/java/sweng/group/one/client_app_desktop/appScenes/SidebarScene.java
+++ src/main/java/sweng/group/one/client_app_desktop/appScenes/SidebarScene.java
@@ -5,10 +5,16 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
+
+import sweng.group.one.client_app_desktop.presentation.Presentation;
 
 public class SidebarScene extends JPanel {
 	private static final long serialVersionUID = 1L;
@@ -17,8 +23,9 @@ public class SidebarScene extends JPanel {
 	// Top of the side bar, containing search bar and minimise button
 	private JPanel top;
 	
-	// Lower portion, containing presentation thumbnails
-	private JPanel thumbs;
+	// Lower portion, containing presentations
+	private JPanel presPanel;
+	private List<Presentation> presentations;
 	
 	private JTextField searchBar;
 	private JButton minimise;
@@ -44,20 +51,17 @@ public class SidebarScene extends JPanel {
 		searchBar.setName("Searchbar");
 		
 		// Add them
-		
 		top.add(minimise, BorderLayout.WEST);
 		top.add(searchBar, BorderLayout.CENTER);
 		
 		// Set the colour (Mostly for demonstration atm)
 		top.setBackground(Color.blue);
 		
-		thumbs = new JPanel();
-		thumbs.setBackground(Color.white);
-		
+		presPanel = new JPanel(new BoxLayout(presPanel, BoxLayout.Y_AXIS));
 		
 		this.setLayout(new BorderLayout());
 		this.add(top, BorderLayout.NORTH);
-		this.add(thumbs, BorderLayout.CENTER);
+		this.add(new JScrollPane(presPanel), BorderLayout.CENTER);
 		this.setName("Sidebar");
 		this.setPreferredSize(new Dimension(200,200));
 	}
@@ -70,10 +74,15 @@ public class SidebarScene extends JPanel {
 		isOpen = false;
 		this.setVisible(false);
 	}
-	public boolean search(String search) {
-		return true;
-	}
 	public boolean isOpen() {
 		return isOpen;
+	}
+	/* Moves P to the top of the presentation stack */
+	public void goTo(Presentation p) {
+		
+	}
+	/* Replaces the current presentation list with p */
+	public void replacePres(List<Presentation> p) {
+		
 	}
 }
