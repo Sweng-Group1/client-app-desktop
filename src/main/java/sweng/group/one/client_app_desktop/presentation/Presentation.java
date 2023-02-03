e90e5c6013fdcfa429011d20b8d40c02e091df15
Fraser Todd <flt515@york.ac.uk>
2023-02-03 17:21:06 +0000

    Made presentation class useable

--- src/main/java/sweng/group/one/client_app_desktop/presentation/Presentation.java
+++ src/main/java/sweng/group/one/client_app_desktop/presentation/Presentation.java
@@ -1,15 +1,42 @@
 package sweng.group.one.client_app_desktop.presentation;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import javax.swing.JPanel;
 
 public class Presentation extends JPanel {
-	private Slide[] slides;
+	private ArrayList<Slide> slides;
 	private int currentSlide;
 	
-	Presentation(){
-		slides = null;
+	public Presentation(ArrayList<Slide> slides){
+		this.slides = slides;
 		currentSlide = 0;
+		showCurrentSlide();
 	}
 	
+	public void addSlide(Slide newSlide) {
+		slides.add(newSlide);
+		this.add(newSlide);
+		newSlide.setVisible(false);
+	}
 	
+	private void showCurrentSlide() {
+		Slide desiredSlide = slides.get(currentSlide);
+		for (Slide slide:slides) {
+			slide.setVisible(slide == desiredSlide);
+		}
+	}
+	
+	public void nextSlide() {
+		int maxSlide = slides.size()-1;
+		currentSlide = (currentSlide + 1) % maxSlide;
+		showCurrentSlide();
+	}
+	
+	public void prevSlide() {
+		int maxSlide = slides.size()-1;
+		currentSlide = (currentSlide + maxSlide - 1) % maxSlide;
+		showCurrentSlide();
+	}
 }
