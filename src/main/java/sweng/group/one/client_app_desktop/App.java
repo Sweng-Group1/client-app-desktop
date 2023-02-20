b556e478f2268baf5a771606b2d54c1a1ef46b82
Fraser Todd <flt515@york.ac.uk>
2023-02-20 17:07:55 +0000

    class creation and initial functionality

--- src/main/java/sweng/group/one/client_app_desktop/App.java
+++ src/main/java/sweng/group/one/client_app_desktop/App.java
@@ -1,5 +1,9 @@
 package sweng.group.one.client_app_desktop;
 
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+import sweng.group.one.client_app_desktop.sceneControl.MapScene;
 
 /**
  * Hello world!
@@ -9,6 +13,15 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
+    	final JFrame frame = new JFrame();
+		frame.setTitle("Map Demo");
+		
+		MapScene ms = new MapScene();
+	    frame.add(ms);
+		frame.pack();
+		frame.setSize(1024, 768);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+       frame.setVisible(true);
     }
 }
