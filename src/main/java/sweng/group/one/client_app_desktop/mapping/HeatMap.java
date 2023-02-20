344a08c85250800128d01d278b5057e6643030b6
Fraser Todd <flt515@york.ac.uk>
2023-02-20 18:29:39 +0000

    added heatmap functionality & marker panning functionality

--- src/main/java/sweng/group/one/client_app_desktop/mapping/HeatMap.java
+++ src/main/java/sweng/group/one/client_app_desktop/mapping/HeatMap.java
@@ -1,5 +1,100 @@
 package sweng.group.one.client_app_desktop.mapping;
 
-public class HeatMap {
+import java.awt.AlphaComposite;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
 
+import org.mapsforge.core.graphics.Canvas;
+import org.mapsforge.core.model.BoundingBox;
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.core.model.Point;
+import org.mapsforge.core.util.MercatorProjection;
+import org.mapsforge.map.awt.graphics.AwtBitmap;
+import org.mapsforge.map.awt.view.MapView;
+import org.mapsforge.map.layer.Layer;
+public class HeatMap extends Layer {
+    private static final String CIRCLEPIC_PATH = "./assets/map/bolilla.png";
+    private static final String GRADIENT_PATH = "./assets/map/colors.png";
+    private static final int HEAT_RADIUS_METERS = 100;
+    private BufferedImage image;
+    private BufferedImage gradient;
+    private ArrayList<EventMarker> markers;
+
+    public HeatMap(ArrayList<EventMarker> markers, MapView mapView) {
+    	super();
+    	try {
+    		image = ImageIO.read(new File(CIRCLEPIC_PATH));
+    		gradient = ImageIO.read(new File(GRADIENT_PATH));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	this.setDisplayModel(mapView.getModel().displayModel);
+    	this.markers = markers;
+    	
+    	for (EventMarker m : markers) {
+    	}
+    }
+    
+    @Override
+    public synchronized void draw(BoundingBox boundingBox, byte zoomLevel, Canvas canvas, Point topLeftPoint) {
+    	int width = canvas.getWidth();
+    	int height = canvas.getHeight();
+    	BufferedImage heatmap = new BufferedImage(width, height, 2); //type 2 is ARGB
+        Graphics2D g = heatmap.createGraphics();
+        
+        //scale image relative to size of the map
+        long mapSize = MercatorProjection.getMapSize(zoomLevel, this.displayModel.getTileSize());
+        double radius = MercatorProjection.metersToPixels(HEAT_RADIUS_METERS, boundingBox.getCenterPoint().latitude, mapSize);
+        radius = (int)radius*2 > 1 ? radius : 1; //ensure a size of atleast 1px
+        Image scaledImage = this.image.getScaledInstance((int)radius*2, (int)radius*2, java.awt.Image.SCALE_SMOOTH);
+        
+        ArrayList<EventMarker> visableMarkers = new ArrayList<>();
+        int maxNumPosts = 0;
+        
+        for(EventMarker m : markers) {
+        	if(!boundingBox.contains(m.getLatLong())) {
+    			continue;
+    		}
+        	visableMarkers.add(m);
+        	int numPosts = m.getNumPosts();
+        	maxNumPosts = numPosts > maxNumPosts ? numPosts : maxNumPosts;
+        }
+    	
+    	for(EventMarker m: visableMarkers) {
+    		LatLong point = m.getLatLong();
+    		if(!boundingBox.contains(point)) { //not the best as wont render something just outside
+    			continue;
+    		}
+	        double pixelX = MercatorProjection.longitudeToPixelX(point.longitude, mapSize);
+	        double pixelY = MercatorProjection.latitudeToPixelY(point.latitude, mapSize);
+	        int left = (int) (pixelX - topLeftPoint.x - radius);
+	        int top = (int) (pixelY - topLeftPoint.y - radius);
+	        
+	        //draw the shape on the map with opacity proportional to the number of posts
+	        g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, m.getNumPosts()/(float)(maxNumPosts*1.1)));
+	        g.drawImage(scaledImage, left, top, null);
+    	}
+    	
+    	//process colour to the heatmap
+    	for (int x = 0; x < width; x++) {
+    		for (int y = 0; y < height; y++) {
+    			final int pixel = heatmap.getRGB(x, y);
+    			if(pixel == 0) { //if the pixel is blank, move on
+    				continue;
+    			}
+    			//new colour based on pixel alpha
+    			final int gradColour = (int)((pixel >>> 24)/255f * (gradient.getHeight()-1)); 
+    			final int newRGB = gradient.getRGB(1, gradColour) & 0x00ffffff; //mask to have no alpha content
+    			heatmap.setRGB(x, y, newRGB | pixel); //give pixel the same alpha as before
+    		}
+    	}
+    	
+    	canvas.drawBitmap(new AwtBitmap(heatmap), 0, 0, width, height, 0, 0, width, height);
+    }
 }
