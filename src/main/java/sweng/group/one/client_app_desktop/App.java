e5b253104a2c7cb7755ecba8e3d718a9a6c08e7c
Oli Partridge <op642@york.ac.uk>
2023-03-09 15:37:00 +0000

    audioPlayer update
    just needs icon resizing, will be complete then.

--- src/main/java/sweng/group/one/client_app_desktop/App.java
+++ src/main/java/sweng/group/one/client_app_desktop/App.java
@@ -53,7 +53,7 @@ public class App
     	slide.add(new DemoElement(new Point(slideX/2-1, slideY/2-1), 10, 10, 0, slide));
     	
     	
-		slide.add(new AudioPlayer(new Point(50, 50), 2, 2, 0, slide, samplemp4, false));
+		slide.add(new AudioPlayer(new Point(0, 0), 2, 2, 0, slide, samplemp4, false));
     	
     	Presentation pres = new Presentation(slides);
     	
--- src/main/java/sweng/group/one/client_app_desktop/media/AudioPlayer.java
+++ src/main/java/sweng/group/one/client_app_desktop/media/AudioPlayer.java
@@ -1,10 +1,9 @@
 package sweng.group.one.client_app_desktop.media;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.net.URL;
 
 import javax.swing.Icon;
@@ -13,10 +12,7 @@ import javax.swing.JPanel;
 import javax.swing.JToggleButton;
 
 import sweng.group.one.client_app_desktop.presentation.Slide;
-import uk.co.caprica.vlcj.player.base.MediaPlayer;
-import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
 import uk.co.caprica.vlcj.player.component.AudioPlayerComponent;
-import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
 
 
 public class AudioPlayer extends PlayableMediaElement{
@@ -24,27 +20,35 @@ public class AudioPlayer extends PlayableMediaElement{
 	JPanel audioPanel;
 	Icon icon;
 	JToggleButton toggleB;
-	//ButtonHandler bHandler = new ButtonHandler();
 	
 	public AudioPlayer(Point pos, int pointWidth, int pointHeight,
 						float duration, Slide slide, URL fileURL, boolean loops) {
 		
 		super(pos, pointWidth, pointHeight, duration, slide, fileURL, loops);
-		this.AudioPlayer = new EmbeddedMediaPlayerComponent();
+		this.AudioPlayer = new AudioPlayerComponent();
 		
-		audioPanel = new JPanel();
-		audioPanel.setBounds(pointHeight, pointHeight, pointWidth, pointHeight);
-		audioPanel.setBackground(Color.blue);
-		slide.add(audioPanel);
-		
+//		audioPanel = new JPanel();
+//		audioPanel.setBounds(pointHeight, pointHeight, pointWidth, pointHeight);
+//		audioPanel.setBackground(Color.blue);
+//		slide.add(audioPanel);
+//		
 		icon = new ImageIcon(".//assets//speaker_icon.png");
 		
 		toggleB = new JToggleButton(icon);
-		toggleB.setFocusPainted(false);
-		//toggleB.addActionListener(bHandler);
-		audioPanel.add(toggleB);
+		component = toggleB;
+		component.setPreferredSize(new Dimension(pointWidth, pointHeight));
+		
+		loadFile();
+		toggleB.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				togglePlaying();
+			}
+			
+		});
 	}
-	private final EmbeddedMediaPlayerComponent AudioPlayer;
+	private final AudioPlayerComponent AudioPlayer;
 
 	@Override
 	public void togglePlaying() {
@@ -65,20 +69,9 @@ public class AudioPlayer extends PlayableMediaElement{
 		AudioPlayer.mediaPlayer().media().startPaused(AudioLocalPath);
 		
 	}
-	
-	
 
 }
 
-//public class ButtonHandler implements ActionListener{
-//	
-//	public void actionPerformed(ActionEvent event){
-//		
-//		AudioButton.loadFile();
-//		AudioButton.togglePlaying();
-//							
-//	}
-//}
 
 
 
