a690e2e1234b8ae4c77167b8f3440a3d7ea23018
ssophiemaw <97915175+ssophiemaw@users.noreply.github.com>
2023-03-12 21:10:07 +0000

    Visual
    Added some visual stuff

--- src/main/java/sweng/group/one/client_app_desktop/App.java
+++ src/main/java/sweng/group/one/client_app_desktop/App.java
@@ -1,9 +1,14 @@
 package sweng.group.one.client_app_desktop;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.IOException;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 import sweng.group.one.client_app_desktop.sceneControl.LoginScene;
 
@@ -13,18 +18,24 @@ import sweng.group.one.client_app_desktop.sceneControl.LoginScene;
  */
 public class App extends JFrame
 {
+	static final Dimension screenSize= Toolkit.getDefaultToolkit().getScreenSize();
     private static final long serialVersionUID = 1L;
 
-	public App()
+	public App() throws IOException
     {
     	setLayout(new BorderLayout());
     	LoginScene loginScene = new LoginScene();
     	add(loginScene, BorderLayout.CENTER);
+    	loginScene.setSize(450, 500);
+    	loginScene.setLocation((int)screenSize.getWidth()/3,(int)screenSize.getHeight()/6);
+		//loginScene.setBackground(new Color(28,34,56));
     }
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		JFrame frame = new App();
-		frame.setSize(300, 150);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);	
+		frame.setLayout(null);
+		frame.setSize((int)screenSize.getWidth(), (int) screenSize.getHeight());
 		frame.setVisible(true);
 	}
 }
--- src/main/java/sweng/group/one/client_app_desktop/sceneControl/LoginScene.java
+++ src/main/java/sweng/group/one/client_app_desktop/sceneControl/LoginScene.java
@@ -1,14 +1,15 @@
 package sweng.group.one.client_app_desktop.sceneControl;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -19,81 +20,168 @@ public class LoginScene extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private static int WINDOW_X_SIZE = 200;
-	private static int WINDOW_Y_SIZE = 200;
-	private static Color textFieldPanelColor = Color.white;
-	private static Color buttonPanelColor = Color.white;
+	//private static int WINDOW_X_SIZE = 200;
+	//private static int WINDOW_Y_SIZE = 200;
+	//private static Color textFieldPanelColor = Color.white;
+	//private static Color buttonPanelColor = Color.white;
 	
-	private JPanel textFieldPanel; 
-	private JPanel buttonPanel;
+	//private JPanel textFieldPanel; 
+	//private JPanel buttonPanel;
 	
+	private JPanel usernamePanel;
+	private JPanel passwordPanel;
+	private JLabel logoPanel;
+	private BufferedImage logo;
 	private JTextField usernameField;
 	private JPasswordField passwordField;
 	
 	private JButton loginButton;
 	private JButton createAccountButton;
-	
+	private JButton continueButton;
 	private String username;
 	private String password;
 	private boolean isOpen;
 	
-	public LoginScene() {
-		setupGUI();
+	public LoginScene() throws IOException {
+		this.setOpaque(false);
+		this.setLayout(null);
+		createUserNameInput();
+		createPasswordField();
+		createButtons();
+		createLogo();
+	}
+	private void createUserNameInput() {
+		usernamePanel= new JPanel() {
+			public void paint(Graphics g) {
+				g.setColor(new Color(160,203,213));
+				//g.setColor(Color.white);
+				g.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), 30, 30);
+				super.paint(g);
+			}
+		};
+		usernameField= new JTextField();
+		usernamePanel.add(usernameField);
+		usernamePanel.setLayout(null);
+		usernamePanel.setOpaque(false);
+		usernameField.setOpaque(false);
+		usernameField.setBackground(new Color(0,0,0,0));
+		usernameField.setBorder(null);
+		usernameField.setCaretColor(Color.white);
+		usernameField.setSelectedTextColor(Color.white);
+		usernameField.setDisabledTextColor(Color.white);
+		usernameField.setForeground(Color.white);
+		this.add(usernamePanel);
+		usernameField.setName("usernameField");
+		usernameField.setVisible(true);
+	}
+	private void createPasswordField() {
+		passwordPanel= new JPanel() {
+			public void paint(Graphics g) {
+				g.setColor(new Color(160,203,213));
+				//g.setColor(Color.white);
+				g.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), 35, 35);
+				super.paint(g);
+			}
+		};
+		passwordField= new JPasswordField();
+		passwordPanel.add(passwordField);
+		passwordPanel.setLayout(null);
+		passwordPanel.setOpaque(false);
+		passwordField.setOpaque(false);
+		passwordField.setBackground(new Color(0,0,0,0));
+		passwordField.setBorder(null);
+		passwordField.setCaretColor(Color.white);
+		passwordField.setSelectedTextColor(Color.white);
+		passwordField.setDisabledTextColor(Color.white);
+		passwordField.setForeground(Color.white);
+		this.add(passwordPanel);
+		passwordField.setName("usernameField");
+		passwordField.setVisible(true);
+	}
+	private void createButtons() {
+		loginButton= new JButton() {	
+			public void paint(Graphics g) {
+				g.setColor(new Color(160,203,213));
+				g.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), 30, 30);
+				g.setColor(Color.white);
+				g.drawString("Login",(this.getWidth()/2)-(g.getFontMetrics().
+						stringWidth("Login")/2),(this.getHeight()/2)+(g.getFontMetrics().getHeight()/4));
+				super.paint(g);
+			}
+		};	
+		loginButton.setOpaque(false);
+		loginButton.setBackground(new Color(0,0,0,0));
+		loginButton.setBorderPainted(false);
+		loginButton.setBorder(null);	
+		this.add(loginButton);
+		
+		continueButton= new JButton(){	
+			public void paint(Graphics g) {
+				g.setColor(new Color(160,203,213));
+				g.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), 30, 30);
+				g.setColor(Color.white);
+				g.drawString("Continue",(this.getWidth()/2)-(g.getFontMetrics().
+						stringWidth("Continue")/2),(this.getHeight()/2)+(g.getFontMetrics().getHeight()/4));
+				super.paint(g);
+			}
+		};	
+		continueButton.setOpaque(false);
+		continueButton.setBackground(new Color(0,0,0,0));
+		continueButton.setBorderPainted(false);
+		continueButton.setBorder(null);	
+		this.add(continueButton);
+		
+		createAccountButton= new JButton(){	
+			public void paint(Graphics g) {
+				g.setColor(new Color(160,203,213));
+				//g.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), 50, 50);
+				//g.setColor(Color.white);
+				g.drawString("Create Account",(this.getWidth()/2)-(g.getFontMetrics().
+						stringWidth("Create Account")/2),(this.getHeight()/2)+(g.getFontMetrics().getHeight()/4));
+				super.paint(g);
+			}
+		};	
+		createAccountButton.setOpaque(false);
+		createAccountButton.setBackground(new Color(0,0,0,0));
+		createAccountButton.setBorderPainted(false);
+		createAccountButton.setBorder(null);	
+		this.add(createAccountButton);
+		
+		
+	}
+	private void createLogo() throws IOException {
+		logoPanel= new JLabel();
+		logo= ImageIO.read(new File("./Yusu Logo 14.png"));
+		this.add(logoPanel);
+	}
+	public void setSize(int width, int height) {
+		super.setSize(width, height);
+		usernamePanel.setSize((width/6)*4, height/16);
+		usernamePanel.setLocation((width-((width/6)*4))/2, (height/2)-(height/16));
+		usernameField.setSize(usernamePanel.getWidth()-20, usernamePanel.getHeight());
+		usernameField.setLocation(10, 0);
+		
+		passwordPanel.setSize((width/6)*4, height/16);
+		passwordPanel.setLocation((width-((width/6)*4))/2, (height/2)+(height/16));
+		passwordField.setSize(usernamePanel.getWidth()-20, usernamePanel.getHeight());
+		passwordField.setLocation(10, 0);
+		
+		continueButton.setSize((passwordPanel.getWidth()/12)*5, passwordPanel.getHeight());
+		continueButton.setLocation(passwordPanel.getX(), (height/2)+(3*(height/16)));
+		
+		loginButton.setSize((passwordPanel.getWidth()/12)*5, passwordPanel.getHeight());
+		loginButton.setLocation(passwordPanel.getX()+(passwordPanel.getWidth()/12)*7, (height/2)+(3*(height/16)));
+		
+		createAccountButton.setSize((passwordPanel.getWidth()/12)*5, passwordPanel.getHeight());
+		createAccountButton.setLocation(passwordPanel.getX(), (height/2)+(5*(height/16)));
+	
+		logoPanel.setIcon(new ImageIcon(logo.getScaledInstance( (height/2)-(height/16), (height/2)-(height/16), java.awt.Image.SCALE_SMOOTH)));
+		logoPanel.setLocation( (width/2)-( ((height/2)-(height/16))/2),0);
+		logoPanel.setSize(passwordPanel.getWidth(), (height/2)-(height/16));
 	}
 	
-	private void setupGUI() {
-		textFieldPanel = new JPanel(new GridLayout(2,1,10,10));
-		buttonPanel = new JPanel(new FlowLayout());
-		
-		usernameField = new JTextField();
-		passwordField = new JPasswordField();
-		
-		loginButton = new JButton("Login");
-		createAccountButton = new JButton("Create Account");
-		//continueButton = new JButton("Continue");
-		
-		textFieldPanel.setName("textFieldPanel");
-		buttonPanel.setName("buttonPanel");
-		
-		usernameField.setName("usernameField");
-		passwordField.setName("passwordField");
-		
-		loginButton.setName("loginButton");
-		createAccountButton.setName("createAccountButton");
-		//continueButton.setName("continueButton");
-		
-		loginButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				login();
-			}
-		});
-		
-		createAccountButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				createAccount();
-			}
-		});
-		
-		textFieldPanel.add(usernameField);
-		textFieldPanel.add(passwordField);
-		
-		buttonPanel.add(loginButton, BorderLayout.EAST);
-		buttonPanel.add(createAccountButton, BorderLayout.WEST);
-		//textFieldPanel.add(continueButton, BorderLayout.SOUTH);
-		
-		textFieldPanel.setBackground(textFieldPanelColor);
-		buttonPanel.setBackground(buttonPanelColor);
-		
-		this.setLayout(new BorderLayout());
-		this.add(textFieldPanel, BorderLayout.NORTH);
-		this.add(buttonPanel, BorderLayout.SOUTH);
-		this.setName("LoginScene");
-		this.setPreferredSize(new Dimension(WINDOW_X_SIZE,WINDOW_Y_SIZE));
-		this.setVisible(true);
-	}
-
+	
+	
 	public void login() {
 		usernameField.setText("The Login Button Works");
 		System.out.println("The Login Button Works");
@@ -106,6 +194,12 @@ public class LoginScene extends JPanel {
 	
 	public boolean isOpen() {
 		return isOpen;
+	}
+	public void paint(Graphics g) {
+		g.setColor(new Color(46,71,118));
+		//g.setColor(Color.white);
+		g.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), 100, 100);
+		super.paint(g);
 	}
 	
 //	private boolean login(String username, String password) {
