d74beb840fe44a01a72b5b05ff9f0290977c831e
Will Hinton <jwh535@its.york.ac.uk>
2023-05-27 16:52:50 +0100

    Slight neaten up, but this isnt scheduled to be refactored so eh.

--- src/main/java/sweng/group/one/client_app_desktop/uiElements/UploadSceneComponent.java
+++ src/main/java/sweng/group/one/client_app_desktop/uiElements/UploadSceneComponent.java
@@ -16,45 +16,39 @@ import sweng.group.one.client_app_desktop.sceneControl.ComponentInterface;
  *
  */
 public class UploadSceneComponent extends JPanel implements ComponentInterface{
-	 protected Color main;
-	 protected Color secondary;
-	 
-	 
-	 Rectangle r;
-	 
-	 public UploadSceneComponent() {
-		 this.setOpaque(false);
-		 r= new Rectangle();
-		
-	 }
+	protected Color main;
+	protected Color secondary;
+
+	protected Rectangle r;
+
 	
-	public void setMarginBounds(int left, int top, int right,int bottom) {
-		r.setBounds(left, top, right,bottom);
-	
+	public UploadSceneComponent() {
+		this.setOpaque(false);
+		r= new Rectangle();
 	}
-	
-	//OVERIDED METHODS:
+
+	public void setMarginBounds(int left, int top, int right, int bottom) {
+		r.setBounds(left, top, right,bottom);
+	}
+
+	@Override
 	public void paintComponent(Graphics g) {
 		Graphics2D g2= (Graphics2D) g.create();
 		g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-		//g2.setColor(main);
-		
-		//g2.fillRoundRect(r.x, r.y, this.getWidth()-r.width-r.x, this.getHeight()-(r.height)-r.y, curvatureRadius, curvatureRadius);
-		//g2.dispose();
-		
-       
-        int w = getWidth(), h = getHeight();
-        Color color1 = main;
-        Color color2;
-        if(main==colorDark) {
-        	color2 = colorDarkHighlight;
-        }else {
-        	color2 = colorLightHighlight;
-        }
-       
-        GradientPaint gp = new GradientPaint(w/2, 0, color2, w/2, 100, color1);
-       
-        g2.setPaint(gp);
+
+		int w = getWidth();
+
+		Color color1 = main;
+		Color color2;
+		if(main==colorDark) {
+			color2 = colorDarkHighlight;
+		}else {
+			color2 = colorLightHighlight;
+		}
+
+		GradientPaint gp = new GradientPaint(w/2, 0, color2, w/2, 100, color1);
+
+		g2.setPaint(gp);
 		g2.fillRoundRect(r.x, r.y, this.getWidth()-r.width-r.x, this.getHeight()-(r.height)-r.y, curvatureRadius, curvatureRadius);
 		g2.dispose();
 		super.paintComponent(g);
