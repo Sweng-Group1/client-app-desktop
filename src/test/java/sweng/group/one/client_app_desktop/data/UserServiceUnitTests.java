8a8ecbcfb2341eecbe9c2b783b6e4b0d59c77a59
Paul Pickering <paulpickering678@gmail.com>
2023-05-29 15:38:33 +0100

    Revised and added mocking to UserServiceUnitTests

--- src/test/java/sweng/group/one/client_app_desktop/data/UserServiceUnitTests.java
+++ src/test/java/sweng/group/one/client_app_desktop/data/UserServiceUnitTests.java
@@ -3,15 +3,32 @@ package sweng.group.one.client_app_desktop.data;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.io.IOException;
 
+/* TEST STRATEGY
+Unit tests for the service class. 
+Uses MockWebServer (another library by OkHttp, the library used for the requests)
+to mock the server interactions. Mockito is used for mocking User. 
+*/
+ 
+@RunWith(MockitoJUnitRunner.class)
 public class UserServiceUnitTests {
     private MockWebServer server;
     private UserService serviceTest;
+    
+    @Mock
+    private User mockedUser;
     
     @Before
     public void setup() throws IOException {
@@ -21,18 +38,34 @@ public class UserServiceUnitTests {
     }
 
     @Test
-    public void testLoginUsingDefaultAdminCredentialsReturns200OkCode() {
-        server.enqueue(new MockResponse().setResponseCode(200)
-        		.setBody(("{\"access_token\":\"TestAccessToken\",\"refresh_token\":\"TestRefreshToken\"}")));
-        
+    public void SuccessfulLoginSavesAccessAndRefreshTokens() throws IOException {
         String defaultAdminUsername = "sid";
         String defaultAdminPass = "password123";
-        User userTest = new User(defaultAdminUsername);
+        String testAccessToken = "TestAccessToken";
+        String testRefreshToken = "TestRefreshToken";
+
+        server.enqueue(new MockResponse()
+            .setResponseCode(200)
+            .setBody("{\"access_token\":\"" + testAccessToken + "\",\"refresh_token\":\"" + testRefreshToken + "\"}"));
+
+        when(mockedUser.getUsername()).thenReturn(defaultAdminUsername);
+        int statusCode = serviceTest.login(mockedUser, defaultAdminPass);
+
+        verify(mockedUser).saveAccessToken(testAccessToken);
+        verify(mockedUser).saveRefreshToken(testRefreshToken);
+    }
+
+    @Test
+    public void successfulRefreshSavesNewAccessToken() throws IOException {
+        String defaultAdminUsername = "sid";
+        String testAccessToken = "TestAccessToken";
+
+        server.enqueue(new MockResponse()
+            .setResponseCode(200)
+            .setBody("{\"access_token\":\"" + testAccessToken + "\"}"));
         
-        System.out.println("Running login test for correct credentials: ");
-        int statusCode = serviceTest.login(userTest, defaultAdminPass);
-        System.out.println("Status Code: " + statusCode);
-        assertThat(statusCode).isEqualTo(200);
+        int statusCode = serviceTest.refreshAccessToken(mockedUser);
+        verify(mockedUser).saveAccessToken(testAccessToken);
     }
     
     @After
