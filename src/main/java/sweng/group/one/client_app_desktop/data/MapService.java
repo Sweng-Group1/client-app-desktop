a708d41fe4ac4fcedfdcb8ee0237544fcd5336a0
Paul Pickering <paulpickering678@gmail.com>
2023-05-29 17:08:23 +0100

    Tided up and added docstrings to the MapService file

--- src/main/java/sweng/group/one/client_app_desktop/data/MapService.java
+++ src/main/java/sweng/group/one/client_app_desktop/data/MapService.java
@@ -1,16 +1,12 @@
 package sweng.group.one.client_app_desktop.data;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URLEncoder;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
-
-import org.json.JSONObject;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
@@ -18,25 +14,29 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
+/**
+ * @author Paul Pickering
+ *  *Service class for handling all map related server tasks, i.e. uploading, retrieving, deleting, etc. 
+ */
 public class MapService {
-	
 	//TODO: Do we want to change these URLs to constant specified elsewhere perhaps?
-	private final static String MAP_URL = "http://localhost:8080/api/v1/map";
-	
-	
-	public int retrieveMap(String name) {
-		
+	private final static String MAP_URL = "http://localhost:8080/api/v1/map/";
+	private static final String MAP_FOLDER = "./assets/map/";
+	/**
+     * Gets a map file from the server. 
+     * @param name The name of the map file, i.e. "York". 
+     * @return Returns the path to the map file. 
+     */
+	public Path retrieveMap(String name) {
 		int statusCode = 0;
-		
+	
 		OkHttpClient client = new OkHttpClient();
-		
 		Request request = new Request.Builder()
 				.url(MAP_URL + name)
 				.get()
 				.build();
-		
-		System.out.println(request.toString());
 		
 		try {
 		    Response response = client.newCall(request).execute();
@@ -46,38 +46,40 @@ public class MapService {
 	
 		    if (statusCode == 403) {
 		        System.out.println("Error: Server returned 403 forbidden");
-		        return statusCode;
+		        return null;
+		    }
+		    
+		    else if (statusCode == 417) {
+		    	System.out.println("Error: server returned 417 expectation failed.");
+		    	return null;
 		    }
 	
-		    // Save the response body as a JSON for easier parsing
-		    String responseBody = response.body().string();
-		    JSONObject json = new JSONObject(responseBody);
-	
-		    // Print the status code and response body
 		    System.out.println("Status code: " + statusCode);
-		    System.out.println("Response body: " + json.toString());
 		    
-		    //TODO: Handle what to do with the Map. Do we want this stored in a "temp" folder?
-		    // Or in a permanant one to avoid redownloads? If so, where?
-	
-	
+		    // Save the map locally. 
+		    Path mapPath = Paths.get(MAP_FOLDER + name + ".map");
+		    Files.createFile(mapPath);
+		    ResponseBody body = response.body();
+		    Files.write(mapPath,body.bytes());
+		    return mapPath;
+		    
+		} catch (FileAlreadyExistsException e) {
+			Path mapPath = Paths.get(MAP_FOLDER + name + ".map");
+			return mapPath;
 		} catch (IOException e) {
 		    // Handle IOException (e.g., network error)
 		    e.printStackTrace();
+		    return null;
 		}
-			 
-			 return statusCode;
-	
 	}
-		
 	
-	// Uploads a map to the server. Returns 200 if successful, 
-	// 403 for incorrect authorisation, 
-	// 400 for an issue with the request parameters, 
-	// or 0 for IO exceptions. 
-	
-	public int uploadMap(Map map, String authToken) {
-		
+	/**
+     * Uploads a map file to the server. 
+     * @param map The map file. 
+     * @param accessToken the authorisation token. 
+     * @return Returns the status code (e.g. 200 success, 403 forbidden). Returns 0 if exception occurred.  
+     */
+	public int uploadMap(Map map, String accessToken) {
 		String filepath = map.getFile().getPath();
 		byte[] fileAsBytes;
 		try {
@@ -89,7 +91,6 @@ public class MapService {
 		}
 		
 		OkHttpClient client = new OkHttpClient();
-		
 		String nameKey = "name";
 		String nameValue = map.getName();
         
@@ -108,10 +109,10 @@ public class MapService {
         Request request = new Request.Builder()
         		.url(MAP_URL)
         		.header("Content-Type", "application/x-www-form-urlencoded")
-        		.header("Authorization", "Bearer " + authToken)
+        		.header("Authorization", "Bearer " + accessToken)
         		.post(requestBody)
         		.build();
-        
+ 
         try {
 			Response response = client.newCall(request).execute();
 			System.out.println("Attempted to upload map! Response Code: " + response.code()
@@ -122,5 +123,4 @@ public class MapService {
 			return 0;
 		}
 	}
-
 }
