ecc235df178621512997520390d3b16b3090250a
Fraser Todd <flt515@york.ac.uk>
2023-06-03 17:06:25 +0100

    fixed weird changes to PresElement

--- src/main/java/sweng/group/one/client_app_desktop/presentation/PresElement.java
+++ src/main/java/sweng/group/one/client_app_desktop/presentation/PresElement.java
@@ -1,10 +1,15 @@
 package sweng.group.one.client_app_desktop.presentation;
 
 import java.awt.Point;
-import java.awt.image.BufferedImage;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JComponent;
 
+/**
+ * @author flt515
+ *
+ */
 public abstract class PresElement{
 	protected Point pos;
 	protected int width;
@@ -12,8 +17,7 @@ public abstract class PresElement{
 	protected float duration;
 	protected Slide slide;
 	protected JComponent component;
-	protected String type;
-
+	protected Timer displayTimer;
 	
 	protected PresElement(Point pos, 
 						int width, 
@@ -26,20 +30,21 @@ public abstract class PresElement{
 		this.height = height;
 		this.duration = duration;
 		this.slide = slide;
-		
+		this.displayTimer = new Timer();
 	}
 	
 	/**
-	 * Method to which makes the element visible for a given time
-	 * specified as "duration"
-	 * The element will not disappear if the duration > 0
-	 *
+	 * Sets the visibility of the component based on the provided boolean value.
+	 * If the component is set to be displayed and a positive duration is specified,
+	 * it will automatically hide the component after the specified duration in seconds.
+	 * @param displaying true if the component should be displayed, false otherwise
 	 */
-	protected void displayElement() {
-		component.setVisible(true);
-		if (duration > 0) {
-			new java.util.Timer().schedule( 
-			        new java.util.TimerTask() {
+	protected void displayElement(boolean displaying) {
+		component.setVisible(displaying);
+		if (duration > 0 && displaying) {
+			displayTimer = new Timer();
+			displayTimer.schedule( 
+			        new TimerTask() {
 			            @Override
 			            public void run() {
 			                component.setVisible(false);
@@ -48,39 +53,13 @@ public abstract class PresElement{
 			        (long) (duration*1000) 
 			);
 		}
-	}
-	public String getType() {
-		return type;
-	}
-	public JComponent getComponent() {
-		return component;
-	}
-	public int getWidth() {
-		return width;
-	}
-	public int getHeight() {
-		return height;
-	}
-	public float getDuration() {
-		return duration;
-	}
-	public Point getPosPoint() {
-		return pos;
-	}
-	public void setX(int x) {
-		pos.setLocation(x, pos.y);
-	}
-	public void setY(int y) {
-		pos.setLocation(pos.x, y);
-	}
-	public void setWidth(int width) {
-		this.width=width;
-	}
-	public void setHeight(int height) {
-		this.height=height;
-	}
-	public void setDuration(float duration) {
-		this.duration=duration;
+		else {
+			displayTimer.cancel();
+			displayTimer.purge();
+		}
 	}
 	
+	protected void displayElement() {
+		this.displayElement(true);
+	}
 }
