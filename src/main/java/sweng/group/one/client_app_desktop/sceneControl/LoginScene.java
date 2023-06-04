da2bdf59c72aa0ea878f331667e71ab68948ed76
Jonathan Cooke <jac696@its.york.ac.uk>
2023-06-04 03:44:25 +0100

    Refactored Login Scene
    Added inner classes for the buttons and textfields to reduce repeated
    code. Improved repaint mechanics and implemented button behaviour  in
    anonymous class. Changed the naming and ordering of buttons. Made it
    actually make some semblance of sense.

--- src/main/java/sweng/group/one/client_app_desktop/sceneControl/LoginScene.java
+++ src/main/java/sweng/group/one/client_app_desktop/sceneControl/LoginScene.java
@@ -6,8 +6,15 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.LayoutManager;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputMethodEvent;
+import java.awt.event.InputMethodListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -25,7 +32,7 @@ import sweng.group.one.client_app_desktop.uiElements.RoundedButton;
 /**
  * Modified JPanel that allows users to login or create an account using a chosen username and password
  * 
- * @author Srikanth Jakka & Luke George
+ * @author Srikanth Jakka & Luke George. Sophie Maw, Fraser Todd & Jonathan Cooke.
  * @since 29/05/2023
  *
  */
@@ -40,15 +47,13 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 	private boolean isOpen;
 	
 	private BufferedImage logo;
-	private RoundedButton continueButton;
-	private RoundedButton createAccountButton;
-	private RoundedButton loginButton;
+	private LoginSceneButton continueButton;
+	private LoginSceneButton createAccountButton;
+	private LoginSceneButton loginButton;
 	private JPanel logoPanel;
-	private JPanel passwordPanel;
-	private JPanel usernamePanel;
+	private InputTextPanel passwordPanel;
+	private InputTextPanel usernamePanel;
 	private JPanel feedbackPanel;
-	private JPasswordField passwordField;
-	private JTextField usernameField;
 	private JLabel feedbackMessage;
 	
 	private String accessToken;
@@ -66,8 +71,13 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 	 */
 	public LoginScene() throws IOException {
 		this.setOpaque(false);
-		createUserNameInput();
-		createPasswordField();
+		
+		usernamePanel = new InputTextPanel("Username", false);	
+		this.add(usernamePanel);
+		
+		passwordPanel = new InputTextPanel("Password", true);
+		this.add(passwordPanel);
+		
 		createFeedbackMessage();
 		createButtons();
 		createLogo();
@@ -77,124 +87,134 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 		this.setLayout(this);
 	}
 	
-	private JPanel createTextPanel(JTextField textField) {
-		JPanel panel = new JPanel() {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = -343751050506935580L;
-
-			public void paint(Graphics g) {
-				Graphics2D g2 = (Graphics2D)g;
-				RenderingHints qualityHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-				qualityHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-				g2.setRenderingHints(qualityHints);
-				g2.setColor(colorLight);
-				g2.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), curvatureRadius/2, curvatureRadius/2);
-				super.paint(g);
-			}
-		};
-		
-		panel.add(usernameField);
-		panel.setLayout(null);
-		panel.setOpaque(false);
-		
-		textField.setOpaque(false);
-		textField.setBackground(transparent);
-		textField.setBorder(null);
-		textField.setCaretColor(Color.white);
-		textField.setSelectedTextColor(Color.white);
-		textField.setDisabledTextColor(Color.white);
-		textField.setForeground(Color.white);
-		this.add(panel);
-		
-		textField.setVisible(true);
-		
-		return panel;
-	}
-	
 	/**
-	 * Creates the username panel that allows the user to input their username, and adds it to LoginScene
+	 * Custom text input box. Follows branded layout and colour scheme.
+	 * 
+	 * @author Jonathan Cooke, Sophie Maw and Fraser Todd
+	 *
 	 */
-	private void createUserNameInput() {
-		usernamePanel = new JPanel() {
+	private class InputTextPanel extends JPanel {
 
-			private static final long serialVersionUID = -343751050506935580L;
-
-			public void paint(Graphics g) {
-				Graphics2D g2 = (Graphics2D)g;
-				g2.setColor(colorLight);
-				RenderingHints qualityHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-				qualityHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-				g2.setRenderingHints(qualityHints);
-				g2.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), curvatureRadius/2, curvatureRadius/2);
-				if(usernameField.getText().isBlank()) {
-					String str = "Username";
-					g2.setColor(colorDark);
-					g2.drawString(str, curvatureRadius/2,(this.getHeight()/2)+(g.getFontMetrics().getHeight()/4));
-				}
-				super.paint(g);
-			}
-		};
-		usernameField = new JTextField();
+		private static final long serialVersionUID = 1L;
+		protected JTextField inputTextField;
 		
-		usernamePanel.add(usernameField);
-		usernamePanel.setLayout(null);
-		usernamePanel.setOpaque(false);
-		
-		usernameField.setOpaque(false);
-		usernameField.setBackground(transparent);
-		usernameField.setBorder(null);
-		usernameField.setCaretColor(Color.white);
-		usernameField.setSelectedTextColor(Color.white);
-		usernameField.setDisabledTextColor(Color.white);
-		usernameField.setForeground(Color.white);
-		this.add(usernamePanel);
-		
-		usernameField.setName("usernameField");
-		usernameField.setVisible(true);
-	}
-	
-	/**
-	 * Creates the password panel that allows the user to input their password, and adds it to LoginScene
-	 */
-	private void createPasswordField() {
-		passwordPanel = new JPanel() {
+		/**
+		 * 
+		 * @param prompt The text to appear in the input box when empty (usually a prompt for the user).
+		 * @param passwordField Set to true to hide user input from the display when typing. False otherwise.
+		 */
+		InputTextPanel(String prompt, Boolean passwordField) {
+			super();
 			
-			private static final long serialVersionUID = -979465226903459677L;
-
-			public void paint(Graphics g) {
-				Graphics2D g2 = (Graphics2D)g;
-				g2.setColor(colorLight);
-				RenderingHints qualityHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-				qualityHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-				g2.setRenderingHints(qualityHints);
-				g2.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), curvatureRadius/2, curvatureRadius/2);
-				if(passwordField.getPassword().length == 0) {
-					String str = "Password";
-					g2.setColor(colorDark);
-					g2.drawString(str, curvatureRadius/2,(this.getHeight()/2)+(g.getFontMetrics().getHeight()/4));
-				}
-				super.paint(g);
+			if (passwordField) {
+				this.inputTextField = new JPasswordField();	
 			}
-		};
-		passwordField = new JPasswordField();
+			else {
+				this.inputTextField = new JTextField();
+			}
+			this.add(inputTextField);
+			this.setLayout(null);
+			this.setOpaque(false);
+			this.setBackground(transparent);
+			
+			inputTextField.setOpaque(false);
+			// Background colour of input text box
+			inputTextField.setBackground(transparent);
+			inputTextField.setBorder(null);
+			// Cursor colour
+			inputTextField.setCaretColor(Color.white);
+			// Input Text Colour When highlighted by the user
+			inputTextField.setSelectedTextColor(Color.white);
+			inputTextField.setDisabledTextColor(Color.white);
+			// Display Text Colour When input by the user
+			inputTextField.setForeground(Color.white);
+			inputTextField.setName(prompt);
+			inputTextField.setVisible(true);
+			}
 		
-		passwordPanel.add(passwordField);
-		passwordPanel.setLayout(null);
-		passwordPanel.setOpaque(false);
+		// Better to use this than overriding paint, as it preserves layering automatically
+		public void paintComponent(Graphics g) {
+			// Advanced 2D geometry
+			Graphics2D g2 = (Graphics2D)g;
+			// Add graphics line smoothing
+			RenderingHints qualityHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			// Set to render based on high quality over performance
+			qualityHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+			g2.setRenderingHints(qualityHints);
+			
+			// Set drawing colour to one the textbox background colour
+			g2.setColor(colorLight);
+			
+			// Button geometry
+			g2.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), curvatureRadius/2, curvatureRadius/2);
+			
+			if(inputTextField.getText().isBlank()) {
+				String str = inputTextField.getName();
+				g2.setColor(colorDark);
+				g2.drawString(str, curvatureRadius/2,(this.getHeight()/2)+(g.getFontMetrics().getHeight()/4));
+			}
+	
+			// Set colour to one of the project defaults
+			g2.setColor(colorLight);
+			
+			super.paintComponent(g);
+			 
+		}
+	}
+	
+	private class LoginSceneButton extends RoundedButton {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private String buttonName;
 		
-		passwordField.setOpaque(false);
-		passwordField.setBackground(transparent);
-		passwordField.setBorder(null);
-		passwordField.setCaretColor(Color.white);
-		passwordField.setSelectedTextColor(Color.white);
-		passwordField.setDisabledTextColor(Color.white);
-		passwordField.setForeground(Color.white);
-		this.add(passwordPanel);
+		public LoginSceneButton(String buttonName, Image image, int curveRadius, Color mainColour, Color pressedColour,
+				Color hoverColour) {
+			super(image, curveRadius, mainColour, pressedColour, hoverColour);
+			this.buttonName = buttonName;
+		}
 		
-		passwordField.setName("passwordField");
-		passwordField.setVisible(true);
+		/**
+		 * Defines LoginScene buttons with different colours to the default scheme.
+		 * 
+		 * @author Jonathan Cooke, Sophie Maw and Fraser Todd
+		 * 
+		 * @param buttonName Will appear as text within the button.
+		 * @param mainColour Primary background colour.
+		 * @param pressedColour Background colour when clicked on.
+		 * @param hoverColour Background colour when moused over.
+		 */
+		public LoginSceneButton(String buttonName, Color mainColour, Color pressedColour,
+				Color hoverColour) {
+			super(null, curvatureRadius, mainColour, pressedColour, hoverColour);
+			this.buttonName = buttonName;
+		}
+		
+		/**
+		 * LoginScene button that adheres to the default colour scheme.
+		 * 
+		 * @param buttonName Will appear as text within the button.
+		 */
+		public LoginSceneButton(String buttonName) {
+			super(null, curvatureRadius, colorLight, colorDark, Color.gray);
+			this.buttonName = buttonName;
+		}
+		
+		// Overriding paint is bad practice, however, this is necessary due to the use of paint overrides in RoundedButton
+		@Override
+		public void paint(Graphics g) {
+			Graphics g2 = g.create();
+			super.paint(g2);
+			String str = buttonName;
+			g.setColor(Color.white);
+			g.drawString(str,(this.getWidth()/2)-(g.getFontMetrics().stringWidth(str)/2),(this.getHeight()/2)+(g.getFontMetrics().getHeight()/4));
+			g.dispose();
+		}
+	}
+	
+	private class FeedbackMessageDisplay extends JPanel {
+		
 	}
 	
 	private void createFeedbackMessage() {
@@ -204,14 +224,14 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 			 */
 			private static final long serialVersionUID = 4422159517991702385L;
 
-			public void paint(Graphics g) {
+			public void paintComponent(Graphics g) {
 				Graphics2D g2 = (Graphics2D)g;
 				RenderingHints qualityHints = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 				qualityHints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 				g2.setRenderingHints(qualityHints);
 				g2.setColor(colorLight);
 				g2.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), curvatureRadius/2, curvatureRadius/2);
-				super.paint(g);
+				super.paintComponent(g);
 			}
 		};
 		feedbackMessage = new JLabel();
@@ -230,63 +250,31 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 		feedbackMessage.setVisible(false);
 	}
 	
-	/**
-	 * Creates the buttons that the user interacts with to submit their information, and adds them to LoginScene
-	 */
+	
+//	/**
+//	 * Creates the buttons that the user interacts with to submit their information, and adds them to LoginScene
+//	 */
 	private void createButtons() {
-		loginButton = new RoundedButton(null, curvatureRadius, colorLight, colorDark, Color.gray) {
+		loginButton = new LoginSceneButton("Log In") {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 5617284310683026116L;
 
-			private static final long serialVersionUID = -4619941565184679039L;
-
-			@Override
-			public void paint(Graphics g) {
-				Graphics g2 = g.create();
-				super.paint(g2);
-				String str = "Log In";
-				g.setColor(Color.white);
-				g.drawString(str,(this.getWidth()/2)-(g.getFontMetrics().stringWidth(str)/2),(this.getHeight()/2)+(g.getFontMetrics().getHeight()/4));
-				g.dispose();
-			}
-			
 			@Override
 			public void buttonPressed() {
 				loginButtonPressed();
 			}
-		};	
+		};
+		
+		continueButton = new LoginSceneButton("Cancel");
+		createAccountButton = new LoginSceneButton("Create Account", transparent, transparent, Color.gray);
+		
 		this.add(loginButton);
-		
-		continueButton = new RoundedButton(null, curvatureRadius, colorLight, colorDark, Color.gray) {
-			
-			private static final long serialVersionUID = 4841957375392333030L;
-
-			@Override
-			public void paint(Graphics g) {
-				Graphics g2 = g.create();
-				super.paint(g2);
-				String str = "Continue";
-				g.setColor(Color.white);
-				g.drawString(str,(this.getWidth()/2)-(g.getFontMetrics().stringWidth(str)/2),(this.getHeight()/2)+(g.getFontMetrics().getHeight()/4));
-				g.dispose();
-			}
-		};	
 		this.add(continueButton);
-		
-		createAccountButton = new RoundedButton(null, curvatureRadius, transparent, transparent, Color.gray) {
-
-			private static final long serialVersionUID = 4840455254538045312L;
-
-			@Override
-			public void paint(Graphics g) {
-				Graphics g2 = g.create();
-				super.paint(g2);
-				String str = "Create Account";
-				g.setColor(colorLight);
-				g.drawString(str,(this.getWidth()-g.getFontMetrics().stringWidth(str))/2,(this.getHeight()/2)+(g.getFontMetrics().getHeight()/4));
-				g.dispose();
-			}
-		};	
 		this.add(createAccountButton);
 	}
+	
 	
 	/**
 	 * Adds the YUSU Logo to LoginScene
@@ -296,10 +284,10 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 		logo = ImageIO.read(new File("./assets/Yusu Logo 14.png"));
 		logoPanel = new JPanel() {
 
-			private static final long serialVersionUID = 6961538361558408979L;
+			//private static final long serialVersionUID = 6961538361558408979L;
 
 			@Override
-			public void paint(Graphics g) {
+			public void paintComponent(Graphics g) {
 				Graphics2D g2 = (Graphics2D)g;
 				int imageSize = Math.min(this.getHeight(), this.getWidth());
 				g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
@@ -310,17 +298,17 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 	}
 	
 	public boolean loginButtonPressed() {		
-		user = new User(usernameField.getText());
-		String password = String.valueOf(passwordField.getPassword());
+		user = new User(usernamePanel.inputTextField.getText());
+		String password = String.valueOf(((JPasswordField) passwordPanel.inputTextField).getPassword());
 		
 		
 		if (user.getUsername().equals("") || password.equals("")) {
-			feedbackMessage.setText("Please enter your login credentials");
+			feedbackMessage.setText("Enter your details");
 			feedbackMessage.setVisible(true);
 		}
 		
 		if (checkAccessToken()==true) {
-			feedbackMessage.setText("You are already logged in!");
+			feedbackMessage.setText("You're already logged in!");
 			feedbackMessage.setVisible(true);
 			return true;
 		}
@@ -329,7 +317,7 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 				System.out.println("User login unsuccessful");
 				feedbackMessage.setText("Invalid login credentials");
 				feedbackMessage.setVisible(true);
-				passwordField.setText("");
+				passwordPanel.inputTextField.setText("");
 				return false;
 			} else if (userService.login(user, password)==200) {
 				System.out.println("User successfully logged in");
@@ -359,11 +347,11 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 	}
 	
 	public String getUsername() {
-		return usernameField.getText();
+		return usernamePanel.inputTextField.getText();
 	}
 	
 	public void createAccount() {
-		usernameField.setText("The Create Account Button Works");
+		usernamePanel.inputTextField.setText("The Create Account Button Works");
 		System.out.println("The Create Account Button Works");
 	}
 	
@@ -372,10 +360,10 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 	}
 	
 	@Override
-	public void paint(Graphics g) {
+	public void paintComponent(Graphics g) {
 		g.setColor(colorDark);
 		g.fillRoundRect(0, 0, this.getWidth(), this.getHeight(), curvatureRadius, curvatureRadius);
-		super.paint(g);
+		super.paintComponent(g);
 	}
 	
 	public RoundedButton getLoginButton() {
@@ -420,16 +408,16 @@ public class LoginScene extends JPanel implements ComponentInterface, LayoutMana
 		int h = getHeight();
 		
 		usernamePanel.setBounds(w/2-w/3, h/2 - h/16, 2*w/3, h/16);
-		usernameField.setBounds(curvatureRadius/2, 0, usernamePanel.getWidth()-20, usernamePanel.getHeight());
+		usernamePanel.inputTextField.setBounds(curvatureRadius/2, 0, usernamePanel.getWidth()-20, usernamePanel.getHeight());
 		
 		passwordPanel.setBounds((w-((w/6)*4))/2, (h/2)+(h/16), (w/6)*4, h/16);
-		passwordField.setBounds(curvatureRadius/2, 0, passwordPanel.getWidth()-20, passwordPanel.getHeight());
+		passwordPanel.inputTextField.setBounds(curvatureRadius/2, 0, passwordPanel.getWidth()-20, passwordPanel.getHeight());
 		
 		feedbackPanel.setBounds((w-((w/6)*4))/2, (h/2)+(6*h/16), (w/6)*4, h/16);
 		feedbackMessage.setBounds(curvatureRadius/2, 0, feedbackPanel.getWidth()-20, feedbackPanel.getHeight());
 
-		continueButton.setBounds(passwordPanel.getX(), (h/2)+(3*(h/16)), (passwordPanel.getWidth()/12)*5, passwordPanel.getHeight());
-		loginButton.setBounds(passwordPanel.getX()+(passwordPanel.getWidth()/12)*7, (h/2)+(3*(h/16)), (passwordPanel.getWidth()/12)*5, passwordPanel.getHeight());
+		loginButton.setBounds(passwordPanel.getX(), (h/2)+(3*(h/16)), (passwordPanel.getWidth()/12)*5, passwordPanel.getHeight());
+		continueButton.setBounds(passwordPanel.getX()+(passwordPanel.getWidth()/12)*7, (h/2)+(3*(h/16)), (passwordPanel.getWidth()/12)*5, passwordPanel.getHeight());
 		createAccountButton.setBounds(passwordPanel.getX(), (h/2)+(5*(h/16)), passwordPanel.getWidth(), passwordPanel.getHeight());
 
 		logoPanel.setBounds(passwordPanel.getX(), 0, passwordPanel.getWidth(), (h/2)-(h/16));
