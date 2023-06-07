875e21ebba294179939f3d0543dc3b74c8de56be
Paul Pickering <paulpickering678@gmail.com>
2023-06-07 19:24:08 +0100

    Fixed warning in custom exception - needed ID

--- src/main/java/sweng/group/one/client_app_desktop/data/AuthenticationException.java
+++ src/main/java/sweng/group/one/client_app_desktop/data/AuthenticationException.java
@@ -2,6 +2,8 @@ package sweng.group.one.client_app_desktop.data;
 
 public class AuthenticationException extends Exception {
 	
+	private static final long serialVersionUID = 1L;
+
 	public AuthenticationException  (String message) {
 		super(message);
 	}
