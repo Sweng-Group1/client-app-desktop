6ec60fefa3d280e9356d71cb054c9e90174c4009
Fraser Todd <flt515@york.ac.uk>
2023-06-08 02:07:00 +0100

    rejigged main

--- src/main/java/sweng/group/one/client_app_desktop/App.java
+++ src/main/java/sweng/group/one/client_app_desktop/App.java
@@ -3,11 +3,8 @@ package sweng.group.one.client_app_desktop;
 import sweng.group.one.client_app_desktop.sceneControl.MainScene;
 
 public class App {
-	
-	private static MainScene mainScene = new MainScene();
-
 	public static void main(String[] args) {
-
+		new MainScene();
 	}
 
 }
